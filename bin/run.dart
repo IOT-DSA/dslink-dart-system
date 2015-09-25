a6f9010785d8ea940fc7e929e84f015970dc44cf
Kenneth Endfinger <kaendfinger@gmail.com>
2015-09-25 12:03:45 -0400

    New Features

--- bin/run.dart
+++ bin/run.dart
@@ -46,6 +46,11 @@ main(List<String> args) async {
       r"$type": "number",
       "?value": Platform.numberOfProcessors
     },
+    "Operating_System": {
+      r"$name": "Operating System",
+      r"$type": "string",
+      "?value": await getOperatingSystemVersion()
+    },
     "Poll_Rate": {
       r"$name": "Poll Rate",
       r"$type": "number",
@@ -68,6 +73,10 @@ main(List<String> args) async {
       r"$name": "Total Memory",
       r"$type": "number",
       "@unit": "mb"
+    },
+    "System_Time": {
+      r"$name": "System Time",
+      r"$type": "string"
     },
     "Free_Memory": {
       r"$name": "Free Memory",
@@ -98,6 +107,14 @@ main(List<String> args) async {
       r"$name": "Free Disk Space",
       r"$type": "number",
       "@unit": "mb"
+    },
+    "Architecture": {
+      r"$type": "string",
+      "?value": await getSystemArchitecture()
+    },
+    "Hostname": {
+      r"$type": "string",
+      "?value": Platform.localHostname
     },
     "Execute_Command": {
       r"$invokable": "write",
@@ -197,6 +214,12 @@ main(List<String> args) async {
     await link.saveAsync();
   });
 
+  Scheduler.every(Interval.TWO_HUNDRED_MILLISECONDS, () {
+    if (systemTimeNode.hasSubscriber) {
+      systemTimeNode.updateValue(new DateTime.now().toString());
+    }
+  });
+
   link.syncValue("/Poll_Rate");
   link.connect();
 }
@@ -215,6 +238,7 @@ SimpleNode usedDiskSpaceNode = link["/Used_Disk_Space"];
 SimpleNode cpuTemperatureNode = link["/CPU_Temperature"];
 SimpleNode processCountNode = link["/Processes"];
 SimpleNode batteryLevelNode = link["/Battery_Level"];
+SimpleNode systemTimeNode = link["/System_Time"];
 
 update([bool shouldScheduleUpdate = true]) async {
   if (shouldScheduleUpdate && timer != null) {
@@ -267,49 +291,6 @@ update([bool shouldScheduleUpdate = true]) async {
   }
 }
 
-Future<Map<String, num>> getDiskUsage() async {
-  if (Platform.isLinux || Platform.isMacOS) {
-    var result = await Process.run("df", const ["/"]);
-    List<String> lines = result.stdout.split("\n");
-    lines.removeWhere((x) => x.trim().isEmpty);
-    String line = lines.last;
-
-    var parts = line.split(" ");
-
-    parts.removeWhere((x) => x.isEmpty);
-
-    var used = int.parse(parts[2]) / 1024;
-    var available = int.parse(parts[3]) / 1024;
-    var total = int.parse(parts[1]) / 1024;
-
-    return {
-      "used": used,
-      "available": available,
-      "total": total,
-      "percentage": (used / total) * 100
-    };
-  } else if (Platform.isWindows) {
-    var result = await Process.run("fsutil", ["volume", "diskfree", "C:"]);
-    List<String> lines = result.stdout.split("\n");
-    int getBytesFor(int n) {
-      return int.parse(lines[n].split(":")[1].trim());
-    }
-
-    var total = getBytesFor(1) / 1024 / 1024;
-    var available = getBytesFor(2) / 1024 / 1024;
-    var used = total - available;
-
-    return {
-      "used": used,
-      "available": available,
-      "total": total,
-      "percentage": (used / total) * 100
-    };
-  }
-
-  return {};
-}
-
 Timer timer;
 
 const Map<String, int> POLL_RATE = const {
@@ -319,282 +300,3 @@ const Map<String, int> POLL_RATE = const {
   "4 seconds": 4000,
   "5 seconds": 5000
 };
-
-File procStatFile = new File("/proc/stat");
-
-Future<double> getCpuUsage() async {
-  if (Platform.isLinux) {
-    Future<List<int>> fetch() async {
-      var lines = await procStatFile.readAsLines();
-      String line = lines.firstWhere((x) {
-        return x.startsWith("cpu ");
-      }, orElse: () => null);
-
-      if (line == null) {
-        return null;
-      }
-
-      var parts = line.split(" ");
-
-      parts.removeWhere((x) => x.isEmpty);
-
-      var user = num.parse(parts[1]);
-      var nice = num.parse(parts[2]);
-      var system = num.parse(parts[3]);
-      var idle = num.parse(parts[4]);
-      var iowait = num.parse(parts[5]);
-      var irq = num.parse(parts[6]);
-      var softrig = num.parse(parts[7]);
-      var steal = num.parse(parts[8]);
-      var used = user + nice + system + irq + softrig + steal;
-      var idlez = idle + iowait;
-
-      return [used, idlez, used + idlez];
-    }
-
-    var first = await fetch();
-    await new Future.delayed(const Duration(milliseconds: 500));
-    var second = await fetch();
-
-    var total = second[2];
-    var oldTotal = first[2];
-    var idle = second[1];
-    var oldIdle = first[1];
-
-    return ((total - oldTotal) - (idle - oldIdle)) / (total - oldTotal) * 100;
-  } else if (Platform.isMacOS) {
-    var result = await Process.run("top", const ["-o", "cpu", "-l", "1", "-stats", "cpu"]);
-    List<String> lines = result.stdout.split("\n");
-    var str = lines.firstWhere((x) => x.startsWith("CPU usage: "), orElse: () => null);
-    if (str == null) {
-      return null;
-    }
-
-    var parts = str.split(" ");
-    var l = parts[2];
-    var r = parts[4];
-
-    l = l.substring(0, l.length - 1);
-    r = r.substring(0, r.length - 1);
-
-    var a = num.parse(l);
-    var b = num.parse(r);
-
-    return a + b;
-  } else if (Platform.isWindows) {
-    return (await getWMICNumber("CPU get LoadPercentage")).toDouble();
-  }
-
-  return 0.0;
-}
-
-Future<num> getFreeMemory() async {
-  if (Platform.isLinux) {
-    try {
-      var result = await Process.run("free", const ["-b"]);
-      List<String> lines = result.stdout.split("\n");
-      var line = lines[1];
-      var parts = line.split(" ");
-
-      parts.removeWhere((x) => x.trim().isEmpty);
-
-      var bytes = num.parse(parts[result.stdout.contains("available") ? 6 : 3]);
-
-      return convertBytesToMegabytes(bytes);
-    } catch (e) {
-      return 0;
-    }
-  } else if (Platform.isMacOS) {
-    var result = await Process.run("vm_stat", []);
-    List<String> lines = result.stdout.split("\n");
-    var firstLine = lines.first;
-    var pageSize = int.parse(firstLine.substring(45, firstLine.lastIndexOf(" bytes)")).trim());
-
-    int get(String n) {
-      var m = lines.firstWhere((x) => x.startsWith(n + ": "), orElse: () => null);
-
-      if (m == null) {
-        return null;
-      }
-
-      m = m.replaceAll(" ", "").replaceAll(".", "");
-      return int.parse(m.split(":").last);
-    }
-
-    var free = get("Pages free");
-    var spec = get("Pages speculative");
-
-    return ((free + spec) * pageSize) / 1024 / 1024;
-  } else if (Platform.isWindows) {
-    return await getWMICNumber("OS get FreePhysicalMemory") / 1024;
-  }
-
-  return 0;
-}
-
-double totalMemoryMegabytes;
-
-int _memSizeBytes;
-
-Future<int> getProcessCount() async {
-  try {
-    var result = await Process.run("ps", Platform.isMacOS ?
-      const ["-A", "-o", "pid"] :
-      const ["-A", "--no-headers"]);
-
-    if (result.exitCode != 0) {
-      throw "Error";
-    }
-
-    return result.stdout.split("\n").length;
-  } catch (e) {
-    return 0;
-  }
-}
-
-Future<bool> hasBattery() async {
-  try {
-    if (Platform.isMacOS) {
-      var result = await Process.run("pmset", ["-g", "batt"]);
-      if (result.exitCode != 0) {
-        return false;
-      }
-
-      if (!result.stdout.contains("Battery")) {
-        return false;
-      }
-
-      return true;
-    } else if (Platform.isWindows) {
-      var result = await getWMICNumber("PATH Win32_Battery Get Availability");
-      return result == 3;
-    }else {
-      return false;
-    }
-  } catch (e) {
-    return false;
-  }
-}
-
-RegExp PERCENTAGE_REGEX = new RegExp(r"([0-9\.]+)%\;");
-
-Future<num> getBatteryPercentage() async {
-  try {
-    if (Platform.isMacOS) {
-      var result = await Process.run("pmset", const ["-g", "batt"]);
-      if (result.exitCode != 0) {
-        throw "Fail";
-      }
-
-      return num.parse(PERCENTAGE_REGEX.firstMatch(result.stdout).group(1));
-    } else if (Platform.isWindows) {
-      return await getWMICNumber("path Win32_Battery Get EstimatedChargeRemaining");
-    } else {
-      return 0;
-    }
-  } catch (e) {
-    return 0;
-  }
-}
-
-Future<int> getMemSizeBytes() async {
-  if (_memSizeBytes != null) {
-    return _memSizeBytes;
-  }
-
-  if (Platform.isMacOS) {
-    var result = await Process.run("sysctl", const ["-n", "hw.memsize"]);
-    _memSizeBytes = int.parse(result.stdout);
-  } else if (Platform.isLinux) {
-    var result = await Process.run("free", const ["-b"]);
-    List<String> lines = result.stdout.split("\n");
-    var line = lines[1];
-    var parts = line.split(" ");
-    parts.removeWhere((x) => x.trim().isEmpty);
-    var bytes = num.parse(parts[1]);
-
-    _memSizeBytes = bytes;
-  } else {
-    _memSizeBytes = await getWMICNumber("ComputerSystem get TotalPhysicalMemory");
-    _memSizeBytes = _memSizeBytes;
-  }
-
-  totalMemoryMegabytes = _memSizeBytes / 1024 / 1024;
-
-  return _memSizeBytes;
-}
-
-Future<bool> doesSupportCPUTemperature() async {
-  if (_supportsCpuTemperature != null) {
-    return _supportsCpuTemperature;
-  }
-
-  if (Platform.isMacOS) {
-    var path = await findExecutable("istats");
-    if (path != null) {
-      return _supportsCpuTemperature = true;
-    }
-  }
-
-  if (Platform.isLinux) {
-    var path = await findExecutable("sensors");
-    if (path != null) {
-      var temp = await getCpuTemp();
-      return _supportsCpuTemperature = temp != 0.0;
-    }
-  }
-
-  return _supportsCpuTemperature = false;
-}
-
-bool _supportsCpuTemperature;
-
-Future<num> getCpuTemp() async {
-  if (Platform.isMacOS) {
-    try {
-      var result = await Process.run("istats", const ["cpu", "temp"]);
-      if (result.exitCode != 0) {
-        return 0.0;
-      }
-      List<String> lines = result.stdout.split("\n");
-      return num.parse(lines.first.split(" ")[2].split("°")[0]);
-    } catch (e) {
-      return 0.0;
-    }
-  }
-
-  if (Platform.isLinux) {
-    try {
-      var result = await Process.run("sensors", const ["coretemp-isa-0000"]);
-      if (result.exitCode != 0) {
-        return 0.0;
-      }
-      List<String> lines = result.stdout.split("\n");
-      String line = lines.firstWhere((x) => x.startsWith("Physical id 0:"), orElse: () => null);
-      if (line == null) {
-        return 0.0;
-      }
-      String x = line.substring("Physical id 0:".length).trim();
-
-      return num.parse(x.split("°")[0]);
-    } catch (e) {
-      return 0.0;
-    }
-  }
-
-  return 0.0;
-}
-
-num convertBytesToMegabytes(num bytes) {
-  return (bytes / 1024) / 1024;
-}
-
-Future<int> getWMICNumber(String query) async {
-  try {
-    var result = await Process.run("wmic", query.split(" "));
-    var lines = result.stdout.split("\n").where((String x) => x.isNotEmpty).skip(1).toList();
-    return int.parse(lines[0]);
-  } catch (e) {
-    return 0;
-  }
-}
